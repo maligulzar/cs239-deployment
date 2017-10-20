a128a6abb36f72f2a62abf0259cd188a2d6a1cc9
Daniel Lau <daniel.s.lau@gmail.com>
2017-10-20 16:38:43 -0700

    initial commit for expertsInLanguages

---
+++ assignments/Assignment2/expertsInLanguages.scala
@@ -0,0 +1,67 @@
+// join posts_answers (a) with posts_questions (q) on a.parent_id and q.id
+
+//  need question information and user id and answer score
+//  each row -> (user id, ([lang1,...langN],score))
+
+// parse relevant text (maybe just tags) for languages
+// reduce by key (user id). put each into form (lang,score). reduce these by lang => (user id, [(lang1,score),...,(langN,score)])
+
+// import org.apache.spark.rdd.PairRDDFunctions
+val pathToQuestions = "/Users/Daniel/IdeaProjects/anothertry/src/main/resources/posts_questions_small.json"
+val pathToAnswers = "/Users/Daniel/IdeaProjects/anothertry/src/main/resources/posts_answers_small.json"
+val languages = List("python","java","scala","c","c++")
+
+val questions = spark.read.format("json").load(pathToQuestions).rdd
+val answers = spark.read.format("json").load(pathToAnswers).rdd
+
+// some rows have owner_user_id==null. 
+// filter all rows that have null values for fields that are going to be used
+val answersFiltered = answers.filter(row => 
+    row.getAs("owner_user_id") != null && 
+    row.getAs("score") != null &&
+    row.getAs("parent_id") != null)
+
+val questionsFiltered = questions.filter( row => 
+    row.getAs("id") != null &&
+    row.getAs("title") != null &&
+    row.getAs("body") != null &&
+    row.getAs("tags") != null)
+
+// use .toString otherwise get java.lang.ClassCastException: java.lang.String cannot be cast to scala.runtime.Nothing$
+// rdd[(String,         List[String])]
+//      question id     unique languages in the title/body/tags
+val questionPairRdd = questionsFiltered.map(row => 
+        (//tuple
+            row.getAs("id").toString,
+            List(row.getAs("title").toString, row.getAs("body").toString, row.getAs("tags").toString)
+                .flatMap(strings => strings.toLowerCase.split("[^\\w+]+"))
+                .filter(languages.contains(_))
+                .toSet.toList
+        )
+    ).filter(row => !row._2.isEmpty) //list of languages is empty
+
+// rdd[(String,     List[String])]
+//      parent_id   owner_user_id, score
+val answerPairRdd = answersFiltered.map(row => 
+        (//tuple
+            row.getAs("parent_id").toString,
+            List(row.getAs("owner_user_id").toString, row.getAs("score").toString)
+        )
+    )
+
+// rdd[(String,         (List[String],      List[String]))]
+//      question_id      languages          owner_user_id,score
+var joinedQuestionAnswer = questionPairRdd.join(answerPairRdd)
+
+// rdd[(String,         (List[String],  String)]
+//      owner_user_id   languages,      score
+var answerLanguagesAndScore = joinedQuestionAnswer.map(row =>  
+        (
+            row._2._2(0), 
+            (
+                row._2._1,
+                row._2._2(1)
+            )
+        )
+    )
+
